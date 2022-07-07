00beede4d8c396c4d9816eecf5f894ab5446e79c
Luke Switzer <luke@rootdowndigital.com>
2022-07-07 11:11:19 -0600

    Initial Commit

---
+++ Shared/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  Shared
+//
+//  Created by Luke on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
